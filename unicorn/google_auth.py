aec5fcd2586e3e422e3a545d0f0e58fb19105814
Robin Duda <robin.duda@idainfront.se>
2018-11-17 16:21:05 +0100

    Workaround for oauth2client bug with sequential auth.

--- unicorn/google_auth.py
+++ unicorn/google_auth.py
@@ -4,6 +4,7 @@ from oauth2client import tools
 import argparse
 import os.path
 import httplib2
+import socket
 
 API_YOUTUBE = 'youtube'
 API_ANALYTICS = 'analytics'
@@ -16,16 +17,29 @@ SCOPE_ANALYTICS = 'https://www.googleapis.com/auth/analytics ' \
                   'https://www.googleapis.com/auth/analytics.readonly'
 
 
+class Flags:
+    pass
+
+
 def authenticate(api_name):
     """ authenticates using OAUTH2 with the given API. """
     if not os.path.isfile(credentials_file(api_name)):
         # if there is no credentials: run the flow.
         flow = flow_from_clientsecrets(secret_file(api_name),
-                                   scope=get_scope(api_name),
-                                   redirect_uri='http://localhost:8080/auth_return')
+                                   scope=get_scope(api_name))
 
         parser = argparse.ArgumentParser(parents=[tools.argparser])
         flags = parser.parse_args()
+
+        # override default port with a random free port to avoid issues when
+        # a second flow is initiated using the same port.
+        override = Flags()
+        # convert argparse.Namespace to dict to make it writable.
+        override.__dict__ = flags.__dict__
+        # make sure we have a unique port.
+        override.auth_host_port = [free_port()]
+        # avoid having logging disabled.
+        override.logging_level = 'INFO'
 
         storage = Storage(credentials_file(api_name))
         tools.run_flow(flow, storage, flags)
@@ -36,6 +50,15 @@ def authenticate(api_name):
         token.refresh(httplib2.Http())
 
     return token.access_token
+
+
+def free_port():
+    """ grab a free TCP port, we can't use 0 as the oauth2 library grabs the port from the URL."""
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind(('', 0))
+    addr, port = tcp.getsockname()
+    tcp.close()
+    return port
 
 
 def get_scope(api):
